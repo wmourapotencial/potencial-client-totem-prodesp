5b4770b439a206f88c7ff73b333f04b8c192aa3e
Your Name <you@example.com>
2021-06-18 11:21:40 -0300

    update

--- src/common/environment.ts
+++ src/common/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
     socket: {
-        //url: 'http://localhost:9005'
+        url: 'http://localhost:9005'
         //url: 'https://desenv.poupatempo.potencialtecnologia.com.br'
-        url: 'https://homolog.poupatempo.potencialtecnologia.com.br'
+        //url: 'https://homolog.poupatempo.potencialtecnologia.com.br'
     },
     impressora: {
-        //url: 'http://192.168.0.23:8080',
-        url: 'http://localhost:8080'
+        url: 'http://192.168.0.23:8080',
+        //url: 'http://localhost:8080'
     },
     socketPotencial: {
-        //url: '192.168.0.23'
-        url: '127.0.0.1'
+        url: '192.168.0.23'
+        //url: '127.0.0.1'
     }
 }
